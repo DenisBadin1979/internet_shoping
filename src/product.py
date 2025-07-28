810d71386601a4a2482153e763be06bd4fcde5e8
Denis_Badin <denisbadin@yandex.ru>
2025-07-29 03:52:46 +0500

    Домашняя работа 14_2 - 29.07.2025

--- src/product.py
+++ src/product.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Product:
     """Класс предоставления продукта"""
 
@@ -11,5 +14,27 @@ class Product:
         """Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, prod_dict: dict) -> None | Any:
+        """Вызываем класс-метод для добавления нового продукта"""
+        name = prod_dict.get("name")
+        description = prod_dict.get("description")
+        price = prod_dict.get("price")
+        quantity = prod_dict.get("quantity")
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        """Геттер для вызова приватного атрибута цены"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        """Сеттер для установления цены, но нен ниже 0"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = new_price
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,22 @@
+from src.product import Product
+
+
 def test_product_init(product):
     assert product.name == "Продукт_Тест"
     assert product.description == "Описание продукта_тест"
     assert product.price == 1.3
     assert product.quantity == 5
+
+
+def test_new_product():
+    product = Product("ПродТест", "Описание ПродТест", 1.5, 7)
+    product.name = "ПродТест"
+    product.description = "Описание ПродТест"
+    product.price = 1.5
+    product.quantity = 7
+
+
+def test_price_new(capsys, product):
+    product.price = -100
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
